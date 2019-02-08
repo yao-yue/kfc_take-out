bb9290ca6be3c95310dc1c2fa4f11f4b7d7942ce
hbw-cloud <2316570512@qq.com>
2019-02-09 01:23:26 +0800

    :beer: :beers:

---
+++ kfc-takeout/src/components/swiper/swiper.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class swiper extends Component {
+  render() {
+    return (
+      <div>
+        滑块组件
+      </div>
+    )
+  }
+}
--- kfc-takeout/src/containers/App.js
+++ kfc-takeout/src/containers/App.js
@@ -1,19 +1,17 @@
 import React from "react"
-
-import style from "./App.scss"
+import "./app.scss"
 
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
     };
   }
   render() {
     return (
-     <div style={style.wrapper}>
+     <div className="app-wrapper">
        最外层组件
      </div>
     );
