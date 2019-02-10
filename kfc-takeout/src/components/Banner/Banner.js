50ef46243bb13c9ea120359cc51c0163b029f4dd
hbw-cloud <2316570512@qq.com>
2019-02-10 21:36:50 +0800

    banner 组件

--- kfc-takeout/src/components/Banner/Banner.js
+++ kfc-takeout/src/components/Banner/Banner.js
@@ -3,23 +3,17 @@ import clock from '../../assets/image/Banner_clock.png'
 import humbag from '../../assets/image/Banner_humbag.png'
 import coffee from '../../assets/image/Banner_coffee.png'
 import './Banner.scss'
+import '../../assets/style/line.css'
 
 export default class Banner extends Component {
   render() {
     return (
-      <div>
-        <div className="banner-container">
+        <div className="banner-container bLine">
           <div className="banner-item"><img src={clock} alt="预约送餐" /><span>预约送餐</span></div>
           <div className="banner-item"><img src={humbag} alt="团享餐" /><span>团享餐</span></div>
           <div className="banner-item"><img src={coffee} alt="K-Coffe" /><span>K COFFEE</span></div>
+          <div className="banner-circle"></div>
         </div>
-        <div className="box">
-          <div className="home_top">
-            这是内容
-        </div>
-        </div>
-      </div>
-
     )
   }
 }
